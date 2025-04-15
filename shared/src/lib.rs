6927228a3059c07cf16e3d3c3537c27dab73ebb9
Alessandro-Cavaliere <alessandrocavaliere41@gmail.com>
2025-04-15 23:35:09 +0200

    build: :recycle: Removed unused crates in `Cargo.toml` for `shared`

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -225,15 +225,6 @@ pub mod evm_utils {
     use std::process::Command as ProcessCommand;
     use std::str;
 
-    // Dependancies for call_verify_function()
-    use alloy_provider::ProviderBuilder;
-    use alloy_sol_types::sol;
-    use alloy_signer_local::PrivateKeySigner;
-    use anyhow::Context;
-    use std::env;
-    use url::Url;
-    use hex::encode;
-
     /// -------------------------------------------
     /// Executes a cast command and returns the output as a String
     /// -------------------------------------------
@@ -415,40 +406,6 @@ pub mod evm_utils {
         ])?;
 
         Ok(deploy_output)
-    }
-
-    /// -------------------------------------------
-    /// Calls the `verify()` function of the deployed VerifierContract
-    /// -------------------------------------------
-    pub async fn call_verify_function(
-        private_key: &str,
-        verifier_contract_address: &str,
-        public_input: Vec<u8>,
-        seal: Vec<u8>,
-    ) -> anyhow::Result<()> {
-        let rpc_url = env::var("ETH_RPC_URL").context("Impossible to find env var: RPC_URL")?;
-        println!("RPC URL: {}", rpc_url);
-        let url = Url::parse(&rpc_url)?;
-        let pk: PrivateKeySigner = private_key.parse()?;
-        let provider = ProviderBuilder::new().wallet(pk).on_http(url);
-
-        sol!(
-            #[sol(rpc)]
-            VerifierContract,
-            "../contracts/out/VerifierContract.sol/VerifierContract.json"
-        );
-
-        let verifier = VerifierContract::new(verifier_contract_address.parse()?, provider);
-        let verify = verifier.verify(
-            public_input.into(),
-            seal.into()
-        );
-        let calldata_hex = format!("0x{}", encode(&verify.calldata()));
-        fs::write("calldata.txt", &calldata_hex)?;
-        verify.call().await?;
-        println!("Verify function called successfully");
-        
-        Ok(())
     }
 
 
