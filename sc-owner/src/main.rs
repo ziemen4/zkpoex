569148654499c0ceeb6060a364d88648423ecded
Alessandro-Cavaliere <alessandrocavaliere41@gmail.com>
2025-04-15 23:34:51 +0200

    build: :recycle: Removed unused crates in `Cargo.toml` for `sc-owner`

--- sc-owner/src/main.rs
+++ sc-owner/src/main.rs
@@ -1,26 +1,30 @@
+// Environment variable loader (.env support)
 use dotenv::dotenv;
-use hex;
-use serde_json;
-use shared::conditions::hash_program_spec;
-use shared::conditions::MethodSpec;
-use shared::context::hash_context_state;
-use shared::evm_utils;
-use shared::input::AccountData;
-use shared::utils;
+
 use std::fs;
 use tokio;
+use hex;
+use serde_json;
+
+// Shared project modules 
+use shared::{
+    conditions::{
+        hash_program_spec,          
+        MethodSpec,                  
+    },
+    context::hash_context_state,
+    evm_utils,                       
+    utils,                           
+    input::AccountData,             
+};
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
+    
     // Parse CLI arguments
     let matches = utils::parse_cli_args_sc_owner();
-
-    // CLI Arguments are as follows
-    // --private-key: Private key of the wallet
-    // --risc0-verifier-contract-address: Address of the RISC0 verifier contract
-    // --context-state: Path to the context state file
-    // --program-spec: Path to the program specification file
     let private_key = matches.get_one::<String>("private-key").unwrap();
     let risc0_verifier_contract_address = matches
         .get_one::<String>("risc0-verifier-contract-address")
@@ -32,11 +36,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .get_one::<std::path::PathBuf>("program-spec")
         .unwrap();
 
-    // Read the context state and program specification files
+    // Read the context state file
     let context_state_json = fs::read_to_string(context_state_file).expect("Failed to read file");
     let context_state: Vec<AccountData> = serde_json::from_str(&context_state_json)?;
     let context_state_hash: String = hex::encode(hash_context_state(&context_state));
 
+    // Read the program spec file
     let program_spec_json = fs::read_to_string(program_spec_file).expect("Failed to read file");
     let program_spec: Vec<MethodSpec> = serde_json::from_str(&program_spec_json)?;
     let program_spec_hash: String = hex::encode(hash_program_spec(&program_spec));
@@ -65,11 +70,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         &image_id,
     )?;
 
+    // Extract the contract address from the deployment output
     let verifier_address = evm_utils::extract_contract_address(&output)
         .expect("Failed to extract contract address")
         .to_string()
         .trim_start_matches("0x")
         .to_string();
+        
     println!(
         "\nVerifier contract deployed at address: 0x{}",
         verifier_address
@@ -78,14 +85,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-
 #[cfg(test)]
 mod tests {
-    use shared::evm_utils;
     pub const VERIFIER_TESTNET_HOLESKY_ADDRESS : &str = "0xb94aA3E7a1CEFd86B5F439d0Ca34aA9D2c612bd9";
-    use dotenv::dotenv;
-    use alloy_sol_types::{SolType};
-    use alloy_sol_types::sol;
+
+    // Dependancies for call_verify_function()
+    use alloy_provider::ProviderBuilder;
+    use alloy_sol_types::{SolType,sol};
+    use alloy_signer_local::PrivateKeySigner;
+    use anyhow::Context;
+    use std::env;
+    use url::Url;
+    use hex::encode;
+    use std::fs;
 
     sol! {
         struct PublicInput {
@@ -94,6 +106,40 @@ mod tests {
             bytes32 contextStateHash;
             address proverAddress;
         }
+    }
+
+    /// -------------------------------------------
+    /// Calls the `verify()` function of the deployed VerifierContract
+    /// -------------------------------------------
+    pub async fn call_verify_function(
+        private_key: &str,
+        verifier_contract_address: &str,
+        public_input: Vec<u8>,
+        seal: Vec<u8>,
+    ) -> anyhow::Result<()> {
+        let rpc_url = env::var("ETH_RPC_URL").context("Impossible to find env var: RPC_URL")?;
+        println!("RPC URL: {}", rpc_url);
+        let url = Url::parse(&rpc_url)?;
+        let pk: PrivateKeySigner = private_key.parse()?;
+        let provider = ProviderBuilder::new().wallet(pk).on_http(url);
+
+        sol!(
+            #[sol(rpc)]
+            VerifierContract,
+            "../contracts/out/VerifierContract.sol/VerifierContract.json"
+        );
+
+        let verifier = VerifierContract::new(verifier_contract_address.parse()?, provider);
+        let verify = verifier.verify(
+            public_input.into(),
+            seal.into()
+        );
+        let calldata_hex = format!("0x{}", encode(&verify.calldata()));
+        fs::write("calldata.txt", &calldata_hex)?;
+        verify.call().await?;
+        println!("Verify function called successfully");
+        
+        Ok(())
     }
 
     #[tokio::test]
@@ -116,7 +162,7 @@ mod tests {
         println!("Context state hash: 0x{}", hex::encode(input.contextStateHash));
         println!("Prover address: 0x{}", hex::encode(input.proverAddress));
 
-        let _output = evm_utils::call_verify_function(
+        let _output = call_verify_function(
             &private_key,
             VERIFIER_TESTNET_HOLESKY_ADDRESS,
             journal,
